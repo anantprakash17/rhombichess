37371e9d8fb60d54f5779dcbdf53475cdf087a16
Farzan Yazdanjou <farzan.yazdanjou@gmail.com>
2023-10-30 16:00:30 -0400

    Landing page hero section

--- src/client/app/(main)/page.jsx
+++ src/client/app/(main)/page.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { PlayLocalButton, PlayOnlineButton } from '../../components/Buttons';
+import LandingHero from '../../components/LandingHero';
 
 export default async function Home() {
   return (
-    <main className="flex justify-center items-center min-h-screen gap-3">
-      <PlayOnlineButton />
-      <PlayLocalButton />
+    <main className="relative flex justify-center items-center min-h-screen gap-3">
+      <LandingHero />
     </main>
   );
 }
