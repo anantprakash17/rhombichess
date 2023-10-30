01b9cc9eecf555942ae1015d5a4f106b8ed2eb47
Farzan Yazdanjou <farzan.yazdanjou@gmail.com>
2023-10-29 23:01:19 -0400

    Unnecessary session call

--- src/client/app/(main)/page.jsx
+++ src/client/app/(main)/page.jsx
@@ -1,11 +1,7 @@
-import { getServerSession } from 'next-auth';
 import React from 'react';
-import { authOptions } from '../api/auth/[...nextauth]/route';
 import { PlayLocalButton, PlayOnlineButton } from '../../components/Buttons';
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
   return (
     <main className="flex justify-center items-center min-h-screen gap-3">
       <PlayOnlineButton />
