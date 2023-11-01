e216cf3e2d4476e747c19f3449482025756db1e6
Farzan Yazdanjou <farzan.yazdanjou@gmail.com>
2023-11-01 18:45:35 -0400

    Styling tweaks

--- src/client/components/LandingHero.jsx
+++ src/client/components/LandingHero.jsx
@@ -12,7 +12,7 @@ export default function LandingHero() {
           </span>
           <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" /></svg>
         </a>
-        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xltext-white">
+        <h1 className="mb-4 text-6xl font-bold tracking-tight leading-none text-gray-900">
           All-New Strategic Online Chess Variant!
         </h1>
         <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48text-gray-400">
