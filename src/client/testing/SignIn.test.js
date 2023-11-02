14bfb6e52fe6c6b684356ad40e00b7b96f3548c0
bazinagm <bazinagm@mcmaster.ca>
2023-11-01 23:39:45 -0400

    Add more test cases and fix up

--- src/client/testing/SignIn.test.js
+++ src/client/testing/SignIn.test.js
@@ -1,7 +1,7 @@
 
-
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import SignInCard from '../components/SignInCard';
 import { useRouter } from 'next/navigation';
 import * as sic from './configs/SignIn_configs';
@@ -14,8 +14,15 @@ useRouter.mockImplementation(() => ({
   refresh: jest.fn(),
 }));
 
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({}),
+  })
+);
+
 describe('Sign In', () => {
-  it('Successful sign in', async () => {
+
+  it('Sign In - Successful sign in', async () => {
     render(<SignInCard />);
 
     const emailInput = screen.getByPlaceholderText('name@company.com');
@@ -35,6 +42,95 @@ describe('Sign In', () => {
 
     expect(signIn).toHaveBeenCalledWith('credentials', {
       email: sic.U1username,
+      password: sic.U1password,
+      redirect: false,
+      callbackUrl: '/',
+    });
+  })
+
+  it('Sign in - Wrong username and wrong password', async () => {
+    render(<SignInCard />);
+
+    const emailInput = screen.getByPlaceholderText('name@company.com');
+    const passwordInput = screen.getByPlaceholderText('••••••••');
+    const submitButton = screen.getByText('Sign in to your account');
+
+    fireEvent.change(emailInput, { target: { value: sic.wrongUsername } });
+    fireEvent.change(passwordInput, { target: { value: sic.wrongPassword } });
+
+    const signIn = jest.fn().mockResolvedValue({ error: 'Incorrect email or password. Please try again.' });
+    require('next-auth/react').signIn = signIn;
+    
+    require('next-auth/react').signIn = signIn;
+
+    fireEvent.click(submitButton);
+
+    await screen.findByText('Signing in...');
+
+    await screen.findByText('Incorrect email or password. Please try again.');
+
+    expect(screen.getByText('Incorrect email or password. Please try again.')).toBeInTheDocument();
+    
+    expect(signIn).toHaveBeenCalledWith('credentials', {
+      email: sic.wrongUsername,
+      password: sic.wrongPassword,
+      redirect: false,
+      callbackUrl: '/',
+    });
+  })
+
+  it('Sign in - Right username and wrong password', async () => {
+    render(<SignInCard />);
+
+    const emailInput = screen.getByPlaceholderText('name@company.com');
+    const passwordInput = screen.getByPlaceholderText('••••••••');
+    const submitButton = screen.getByText('Sign in to your account');
+
+    fireEvent.change(emailInput, { target: { value: sic.U1username } });
+    fireEvent.change(passwordInput, { target: { value: sic.wrongPassword } });
+
+    const signIn = jest.fn().mockResolvedValue({ error: 'Incorrect email or password. Please try again.' });
+    require('next-auth/react').signIn = signIn;
+
+    fireEvent.click(submitButton);
+
+    await screen.findByText('Signing in...');
+
+    await screen.findByText('Incorrect email or password. Please try again.');
+
+    expect(screen.getByText('Incorrect email or password. Please try again.')).toBeInTheDocument();
+    
+    expect(signIn).toHaveBeenCalledWith('credentials', {
+      email: sic.U1username,
+      password: sic.wrongPassword,
+      redirect: false,
+      callbackUrl: '/',
+    });
+  })
+
+  it('Sign in - Wrong username and right password', async () => {
+    render(<SignInCard />);
+
+    const emailInput = screen.getByPlaceholderText('name@company.com');
+    const passwordInput = screen.getByPlaceholderText('••••••••');
+    const submitButton = screen.getByText('Sign in to your account');
+
+    fireEvent.change(emailInput, { target: { value: sic.wrongUsername } });
+    fireEvent.change(passwordInput, { target: { value: sic.U1password } });
+
+    const signIn = jest.fn().mockResolvedValue({ error: 'Incorrect email or password. Please try again.' });
+    require('next-auth/react').signIn = signIn;
+
+    fireEvent.click(submitButton);
+
+    await screen.findByText('Signing in...');
+
+    await screen.findByText('Incorrect email or password. Please try again.');
+
+    expect(screen.getByText('Incorrect email or password. Please try again.')).toBeInTheDocument();
+    
+    expect(signIn).toHaveBeenCalledWith('credentials', {
+      email: sic.wrongUsername,
       password: sic.U1password,
       redirect: false,
       callbackUrl: '/',
--- src/client/testing/configs/SignIn_configs.js
+++ src/client/testing/configs/SignIn_configs.js
@@ -1,2 +1,5 @@
 export const U1username = 'mbazinagrolinger@gmail.com';
 export const U1password = 'password';
+
+export const wrongUsername = 'wrong@gmail.com';
+export const wrongPassword = 'wrong';
