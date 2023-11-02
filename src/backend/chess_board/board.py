cb867e32b5622efefef57b4428edea1ca1975323
Anant Prakash <anantprakash92@gmail.com>
2023-11-02 17:41:13 -0400

    feat: add ability to move piece from tile

--- src/backend/chess_board/board.py
+++ src/backend/chess_board/board.py
@@ -18,7 +18,7 @@ class chess_tile:
             str: piece on the tile
         """
         return self.piece
-    
+
     def get_color(self) -> int:
         """
         Get the color of the tile
@@ -26,7 +26,7 @@ class chess_tile:
             int: color of the tile
         """
         return self.color
-    
+
     def get_orientation(self) -> int:
         """
         Get the orientation of the tile
@@ -34,10 +34,13 @@ class chess_tile:
             int: orientation of the tile
         """
         return self.orientation
-    
+
     def __str__(self):
-        return f"Piece: {self.piece}, Color: {self.color}, Orientation: {self.orientation}"
-    
+        return (
+            f"Piece: {self.piece}, Color: {self.color}, Orientation: {self.orientation}"
+        )
+
+
 class chess_board:
     def __init__(self):
         """
@@ -45,14 +48,22 @@ class chess_board:
         """
         self.create_board()
 
-        
     def create_board(self) -> None:
         """
         Creates an empty chess board with 17 columns
         """
         # Create half the board columns then duplicate and reverse after adding the middle column
         board = [[] for _ in range(8)]
-        color_list = [[0,1,2], [2,1,0], [1,2,0], [0,2,1], [2,0,1], [1,0,2], [0,1,2], [2,1,0]]
+        color_list = [
+            [0, 1, 2],
+            [2, 1, 0],
+            [1, 2, 0],
+            [0, 2, 1],
+            [2, 0, 1],
+            [1, 0, 2],
+            [0, 1, 2],
+            [2, 1, 0],
+        ]
         min_diag = 6
         min_horiz = 12
         for i in range(8):
@@ -60,11 +71,10 @@ class chess_board:
                 board[i] = self.create_tile_column(color_list[i], min_diag, 0)
                 min_diag += 1
             else:
-                board[i] = (self.create_tile_column(color_list[i], min_horiz, 1))
+                board[i] = self.create_tile_column(color_list[i], min_horiz, 1)
                 min_horiz += 2
 
-
-        board = board + [self.create_tile_column([1,2,0], 10, 1)] + board[::-1]
+        board = board + [self.create_tile_column([1, 2, 0], 10, 1)] + board[::-1]
 
         self.board = board
 
@@ -75,17 +85,35 @@ class chess_board:
             list[list[chess_tile]]: chess board
         """
         return self.board
-    
-    def create_tile_column(self, colors: list[int], size: int, orientation: int) -> list[chess_tile]:
+
+    def create_tile_column(
+        self, colors: list[int], size: int, orientation: int
+    ) -> list[chess_tile]:
         """
         Assigns color to a column of tiles based on given pattern
         Args:
             colors (list): list of colors to assign to column
-            size (int): size 
+            size (int): size
             orientation (int): orientation of the column
         """
-        return [chess_tile("", colors[i % 3], (orientation * (-1) ** i) if orientation == 1 else orientation) for i in range(size)]
-        
+        return [
+            chess_tile(
+                "",
+                colors[i % 3],
+                (orientation * (-1) ** i) if orientation == 1 else orientation,
+            )
+            for i in range(size)
+        ]
+    
+    def move_piece(self, start: tuple[int, int], end: tuple[int, int]) -> None:
+        """
+        Moves a piece from start to end
+        Args:
+            start (tuple): starting position
+            end (tuple): ending position
+        """
+        self.board[end[0]][end[1]].piece = self.board[start[0]][start[1]].piece
+        self.board[start[0]][start[1]].piece = ""
 
 
 board = chess_board()
