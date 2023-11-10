c1375cf8d508b55eb5ff2f105481b19e561dc5d3
nidanasir <nasirn4@mcmaster.ca>
2023-11-10 13:01:56 -0500

    fixed routing, image size, href issue  and typo

--- src/client/next.config.js
+++ src/client/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/rules/:path*',
+          destination: '/api/rules/:path*',
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
