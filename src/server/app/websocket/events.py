65f5c4af1c2b0d6d344ca4e2fd81b403d208386e
Philip <philiphockey009@gmail.com>
2023-11-16 00:02:40 -0500

    updated like one comment

--- src/server/app/websocket/events.py
+++ src/server/app/websocket/events.py
@@ -11,8 +11,11 @@ def handle_disconnect():
 	print("Client disconnected")
 
 
+@socketio.on('new_message')
+def 
+
 @socketio.on('test_event')
 def test_event():
 	print("test event has activated")
-	#return a random response
+	#return a random response to update all clients listening on the websocket
 	emit('response', {'json_time' : 'its go time'})
