5495032f82436323eab054d6c82828c010f6f83c
bazinagm <bazinagm@mcmaster.ca>
2023-11-17 09:12:08 -0500

    fix online play screen

--- src/client/components/Buttons.jsx
+++ src/client/components/Buttons.jsx
@@ -21,42 +21,26 @@ export function PlayOnlineButton() {
   );
 }
 
+export function PlayLocalButton() {
+  return (
+    <button className="text-3xl rounded-lg font-semibold bg-blue-400 text-white px-6 py-4 hover:bg-blue-500 focus:bg-blue-600" type="button" onClick={() => {}}>
+      Play Local
+    </button>
+  );
+}
+
 export function CreateLobbyButton() {
   return (
-    <button className="text-3xl rounded-lg font-semibold bg-green-500 text-white px-6 py-4 hover:bg-green-600 focus:bg-green-700" type="button" onClick={() => {}}>
-      Create
+    <button className="w-full rounded-lg bg-blue-500 px-5 py-2.5 text-center font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:cursor-not-allowed" type="button" onClick={() => {}}>
+      Start New Game
     </button>
   );
 }
 
 export function JoinExistingLobbyButton() {
   return (
-    <button className="text-3xl rounded-lg font-semibold bg-green-500 text-white px-6 py-4 hover:bg-green-600 focus:bg-green-700" type="button" onClick={() => {}}>
-      Join
-    </button>
-  );
-}
-
-export function CreateLobbyPageButton({ onClick }) {
-  return (
-    <button className="text-3xl rounded-lg font-semibold bg-blue-400 text-white px-6 py-4 hover:bg-blue-500 focus:bg-blue-600" type="button" onClick={onClick}>
-      Create Lobby
-    </button>
-  );
-}
-
-export function JoinExistingLobbyPageButton({ onClick }) {
-  return (
-    <button className="text-3xl rounded-lg font-semibold bg-blue-400 text-white px-6 py-4 hover:bg-blue-500 focus:bg-blue-600" type="button" onClick={onClick}>
-      Join Existing Lobby
-    </button>
-  );
-}
-
-export function PlayLocalButton() {
-  return (
-    <button className="text-3xl rounded-lg font-semibold bg-blue-400 text-white px-6 py-4 hover:bg-blue-500 focus:bg-blue-600" type="button" onClick={() => {}}>
-      Play Local
+    <button className="w-full rounded-lg bg-blue-500 px-5 py-2.5 text-center font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:cursor-not-allowed" type="button" onClick={() => {}}>
+      Join Game
     </button>
   );
 }
