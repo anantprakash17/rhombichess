ec04bd0364212b7f09d2fa5da6673aaaa9dce51d
bazinagm <bazinagm@mcmaster.ca>
2023-11-18 02:24:09 -0500

    link game code to its route

--- src/client/components/Buttons.jsx
+++ src/client/components/Buttons.jsx
@@ -37,11 +37,13 @@ export function CreateLobbyButton() {
   );
 }
 
-export function JoinExistingLobbyButton() {
+export function JoinExistingLobbyButton({ lobbyCode }) {
   return (
-    <button className="w-full rounded-lg bg-blue-500 px-5 py-2.5 text-center font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:cursor-not-allowed" type="button" onClick={() => {}}>
-      Join Game
-    </button>
+    <a href={`/game/${lobbyCode}`}>
+      <button className="w-full rounded-lg bg-blue-500 px-5 py-2.5 text-center font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:cursor-not-allowed" type="button" onClick={() => {}}>
+        Join Game
+      </button>
+    </a>
   );
 }
 
