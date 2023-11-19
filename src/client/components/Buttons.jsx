8ef487213cb025ee9fc120063097739b9197c3e4
Farzan Yazdanjou <farzan.yazdanjou@gmail.com>
2023-11-18 22:02:02 -0500

    Minor tweaks :)

--- src/client/components/Buttons.jsx
+++ src/client/components/Buttons.jsx
@@ -14,7 +14,7 @@ export function SignOutButton() {
 export function PlayOnlineButton() {
   return (
     <a href="/play/online">
-      <button className="text-3xl rounded-lg font-semibold bg-green-500 text-white px-6 py-4 hover:bg-green-600 focus:bg-green-700" type="button">
+      <button className="text-2xl rounded-lg font-semibold bg-green-500 text-white px-4 py-2 hover:bg-green-600 focus:bg-green-700" type="button">
         Play Online
       </button>
     </a>
@@ -23,7 +23,7 @@ export function PlayOnlineButton() {
 
 export function PlayLocalButton() {
   return (
-    <button className="text-3xl rounded-lg font-semibold bg-blue-400 text-white px-6 py-4 hover:bg-blue-500 focus:bg-blue-600" type="button" onClick={() => {}}>
+    <button className="text-2xl rounded-lg font-semibold bg-blue-400 text-white px-4 py-2 hover:bg-blue-500 focus:bg-blue-600" type="button" onClick={() => {}}>
       Play Local
     </button>
   );
@@ -39,11 +39,10 @@ export function CreateLobbyButton() {
 
 export function JoinExistingLobbyButton({ lobbyCode }) {
   return (
-    <a href={`/game/${lobbyCode}`}>
+    <a href={lobbyCode ? `/game/${lobbyCode}` : undefined}>
       <button className="w-full rounded-lg bg-blue-500 px-5 py-2.5 text-center font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:cursor-not-allowed" type="button" onClick={() => {}}>
         Join Game
       </button>
     </a>
   );
 }
-
