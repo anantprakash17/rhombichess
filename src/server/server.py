8cf2e2336c534881fd5d7372d8b99ffd3bb8ceae
bazinagm <bazinagm@mcmaster.ca>
2023-11-19 14:52:44 -0500

    Update server.py

--- src/server/server.py
+++ src/server/server.py
@@ -1,10 +1,6 @@
 from app import app
-from flask import Flask, request
 from flask_socketio import SocketIO, join_room
 from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)  
 
 socketio = SocketIO(app, cors_allowed_origins="http://localhost:3000")
 
