26d5d5fb44b618e8935c93dc5de537979730b0a6
Yazdanjou Farzan <yazdanjf@mcmaster.ca>
2023-11-20 17:30:21 -0500

    Requiring authentication on /game and /play/online routes

--- src/client/app/(main)/game/[gameId]/page.jsx
+++ src/client/app/(main)/game/[gameId]/page.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 import prisma from '../../../../lib/prisma';
 import PageNotFound from '../../../../components/PageNotFound';
 import Game from '../../../../components/Game';
+import { authOptions } from '../../../api/auth/[...nextauth]/route';
 
 export default async function page({ params }) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect('/api/auth/signin'); // TODO: Callback should be current game link
+  }
+
   const { gameId } = params;
 
   // TODO: Once API has been set up, retrieve game information from API
