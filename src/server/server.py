702785bdf32d8c95c08839b443199ee0832a3dd3
bazinagm <bazinagm@mcmaster.ca>
2023-11-19 23:35:59 -0500

    Update server.py

--- src/server/server.py
+++ src/server/server.py
@@ -1,4 +1,13 @@
-from app import app, socketio, join_room
+from flask import Flask, request
+from flask_socketio import SocketIO, join_room
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+socketio = SocketIO(app, cors_allowed_origins="http://localhost:3000")
+
+@app.route('/')
 
 @socketio.on('join_room')
 def handle_join_room(data):
@@ -7,6 +16,7 @@ def handle_join_room(data):
 @socketio.on('send_message')
 def handle_send_message(data):
     room = data.get('room')
+    socketio.emit('receive_message', data, to=room)
 
 if __name__ == '__main__':
     socketio.run(app, port=3001)
