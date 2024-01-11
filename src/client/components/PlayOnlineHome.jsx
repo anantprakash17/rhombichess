bc23f8ef9da737f177fa2b8e41d92315d1be215c
bazinagm <bazinagm@mcmaster.ca>
2024-01-10 23:36:17 -0500

    ask for pw when joining a game

--- src/client/components/PlayOnlineHome.jsx
+++ src/client/components/PlayOnlineHome.jsx
@@ -6,12 +6,17 @@ import baseUrl from '../constants';
 
 export default function PlayOnlineHome() {
   const [showPassword, setShowPassword] = useState(false);
+  const [showExistingPassword, setShowExistingPassword] = useState(false);
   const [gameCode, setgameCode] = useState('');
   const [password, setPassword] = useState('');
+  const [existingGamePassword, setExistingGamePassword] = useState('');
+  const [showGamePasswordField, setShowGamePasswordField] = useState(false);
 
   const handleGameCodeChange = (event) => setgameCode(event.target.value.trim());
 
   const handlePasswordChange = (event) => setPassword(event.target.value.trim());
+
+  const handleExistingGamePasswordChange = (event) => setExistingGamePassword(event.target.value.trim());
 
   const handleCreateGame = async (event) => {
     event.preventDefault();
@@ -33,10 +38,30 @@ export default function PlayOnlineHome() {
     }
   };
 
-  const handleJoinGame = (event) => {
+  const handleJoinGame = async (event) => {
     event.preventDefault();
+
     if (gameCode) {
-      window.location.href = `/game/${gameCode}`;
+      const url = `${baseUrl}/api/game/${gameCode}`;
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      const gamePassword = data?.password;
+
+      if (gamePassword === '') {
+        window.location.href = `/game/${gameCode}`;
+      }
+      else {
+        setShowGamePasswordField(true);
+      }
+
+      if (gamePassword === existingGamePassword) {
+        window.location.href = `/game/${gameCode}`;
+      }
     }
   };
 
@@ -103,6 +128,35 @@ export default function PlayOnlineHome() {
               type="text"
             />
           </div>
+          {showGamePasswordField && (
+            <>
+              <label htmlFor="gameCode" className="mb-2 block font-medium">
+                Game Password
+              </label>
+              <div className="relative">
+                <input
+                  className="mb-4 block w-full rounded-lg border border-slate-300 bg-slate-50 p-2.5 pr-12 placeholder:text-slate-400 focus:border-blue-500 focus:ring-blue-500"
+                  id="password"
+                  name="password"
+                  onChange = {handleExistingGamePasswordChange}
+                  placeholder={showExistingPassword ? 'password' : '••••••••'}
+                  type={showExistingPassword ? 'text' : 'password'}
+                />
+                <button
+                  aria-label={showPassword ? 'Hide password' : 'Show password'}
+                  className="absolute inset-y-0 right-0 my-auto ml-2 mr-2.5 flex h-fit items-center p-1"
+                  onClick={() => setShowExistingPassword(!showExistingPassword)}
+                  type="button"
+                >
+                  {showExistingPassword ? (
+                    <OpenEye />
+                  ) : (
+                    <ClosedEye />
+                  )}
+                </button>
+              </div>
+            </>
+          )}
           <div className="text-center">
             <button className="w-full rounded-lg bg-blue-500 px-5 py-2.5 text-center font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:cursor-not-allowed" type="submit" onClick={() => {}}>
               Join Game
