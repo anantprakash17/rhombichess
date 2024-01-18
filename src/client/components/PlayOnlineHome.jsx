cc4e0a5b985474eee5a3b5957755607c535f0d17
Bazina-Grolinger Monica Tina <bazinagm@mcmaster.ca>
2024-01-17 19:12:51 -0500

    Resolve "Ask for Password when Joining Existing Game"

--- src/client/components/PlayOnlineHome.jsx
+++ src/client/components/PlayOnlineHome.jsx
@@ -6,12 +6,21 @@ import baseUrl from '../constants';
 
 export default function PlayOnlineHome() {
   const [showPassword, setShowPassword] = useState(false);
+  const [showExistingPassword, setShowExistingPassword] = useState(false);
   const [gameCode, setgameCode] = useState('');
-  const [password, setPassword] = useState('');
+  const [creatingPassword, setPassword] = useState('');
+  const [joiningPassword, setJoiningPassword] = useState('');
+  const [showGamePasswordField, setShowGamePasswordField] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const handleGameCodeChange = (event) => setgameCode(event.target.value.trim());
+  const handleGameCodeChange = (event) => {
+    setgameCode(event.target.value.trim());
+    setShowGamePasswordField(false);
+    setErrorMessage('');
+  };
 
-  const handlePasswordChange = (event) => setPassword(event.target.value.trim());
+  const handleCreatingPasswordChange = (event) => setPassword(event.target.value.trim());
+  const handleJoiningPasswordChange = (event) => setJoiningPassword(event.target.value.trim());
 
   const handleCreateGame = async (event) => {
     event.preventDefault();
@@ -22,7 +31,7 @@ export default function PlayOnlineHome() {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ password: password }),
+      body: JSON.stringify({ password: creatingPassword }),
     });
 
     const data = await response.json();
@@ -33,10 +42,34 @@ export default function PlayOnlineHome() {
     }
   };
 
-  const handleJoinGame = (event) => {
+  const handleJoinGame = async (event) => {
     event.preventDefault();
+
     if (gameCode) {
-      window.location.href = `/game/${gameCode}`;
+      const url = `${baseUrl}/api/game/${gameCode}`;
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      const gamePassword = data?.password;
+
+      if (data?.message === 'Game not found') {
+        setErrorMessage('Game not found!');
+      } else if (!gamePassword) {
+        window.location.href = `/game/${gameCode}`;
+      } else {
+        if (!joiningPassword) {
+          setErrorMessage('This game requires a password.');
+        } else if (gamePassword === joiningPassword) {
+          window.location.href = `/game/${gameCode}`;
+        } else if (gamePassword !== joiningPassword) {
+          setErrorMessage('Incorrect code or password. Please try again.');
+        }
+        setShowGamePasswordField(true);
+      }
     }
   };
 
@@ -47,15 +80,15 @@ export default function PlayOnlineHome() {
       </h1>
       <form className="space-y-6 mb-10" onSubmit={handleCreateGame}>
         <div>
-          <label htmlFor="password" className="mb-2 block font-medium">
+          <label htmlFor="creatingPassword" className="mb-2 block font-medium">
             Password (optional)
           </label>
           <div className="relative">
             <input
               className="mb-4 block w-full rounded-lg border border-slate-300 bg-slate-50 p-2.5 pr-12 placeholder:text-slate-400 focus:border-blue-500 focus:ring-blue-500"
-              id="password"
+              id="creatingPassword"
               name="password"
-              onChange = {handlePasswordChange}
+              onChange={handleCreatingPasswordChange}
               placeholder={showPassword ? 'password' : '••••••••'}
               type={showPassword ? 'text' : 'password'}
             />
@@ -87,6 +120,11 @@ export default function PlayOnlineHome() {
       <h1 className="mb-6 text-3xl text-center font-bold tracking-tight leading-none text-gray-900">
         Join an Existing Game
       </h1>
+      {errorMessage && (
+        <div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
+          <span className="font-medium">{errorMessage}</span>
+        </div>
+      )}
       <form className="space-y-6" onSubmit={handleJoinGame}>
         <div>
           <label htmlFor="gameCode" className="mb-2 block font-medium">
@@ -103,6 +141,35 @@ export default function PlayOnlineHome() {
               type="text"
             />
           </div>
+          {showGamePasswordField && (
+            <>
+              <label htmlFor="gameCode" className="mb-2 block font-medium">
+                Game Password
+              </label>
+              <div className="relative">
+                <input
+                  className="mb-4 block w-full rounded-lg border border-slate-300 bg-slate-50 p-2.5 pr-12 placeholder:text-slate-400 focus:border-blue-500 focus:ring-blue-500"
+                  id="joiningPassword"
+                  name="password"
+                  onChange={handleJoiningPasswordChange}
+                  placeholder={showExistingPassword ? 'password' : '••••••••'}
+                  type={showExistingPassword ? 'text' : 'password'}
+                />
+                <button
+                  aria-label={showPassword ? 'Hide password' : 'Show password'}
+                  className="absolute inset-y-0 right-0 my-auto ml-2 mr-2.5 flex h-fit items-center p-1"
+                  onClick={() => setShowExistingPassword(!showExistingPassword)}
+                  type="button"
+                >
+                  {showExistingPassword ? (
+                    <OpenEye />
+                  ) : (
+                    <ClosedEye />
+                  )}
+                </button>
+              </div>
+            </>
+          )}
           <div className="text-center">
             <button className="w-full rounded-lg bg-blue-500 px-5 py-2.5 text-center font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:cursor-not-allowed" type="submit" onClick={() => {}}>
               Join Game
