f2581426940bf92285120891bd5805902b26bfdb
bazinagm <bazinagm@mcmaster.ca>
2024-01-28 16:06:56 -0500

    switch out svgs

--- src/client/components/GameStatsTab.jsx
+++ src/client/components/GameStatsTab.jsx
@@ -20,9 +20,13 @@ export default function GameStatsTab({ gameCode, gamePassword }) {
               {gameCode}
             </span>
             {copiedField === 0 ? (
-              <Image src="/checkmark.svg" alt="Copied" width={15} height={15} className="ml-2" />
+              <svg className="w-6 h-6 text-gray-800 dark:text-white ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m5 12 4.7 4.5 9.3-9"/>
+              </svg>
             ) : (
-              <Image src="/copy.svg" alt="Copy game code" width={15} height={15} className="ml-2" />
+              <svg className="w-6 h-6 text-gray-800 dark:text-white ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" strokeLinejoin="round" strokeWidth="2" d="M9 8v3c0 .6-.4 1-1 1H5m11 4h2c.6 0 1-.4 1-1V5c0-.6-.4-1-1-1h-7a1 1 0 0 0-1 1v1m4 3v10c0 .6-.4 1-1 1H6a1 1 0 0 1-1-1v-7.1c0-.3 0-.5.2-.7l2.5-2.9c.2-.2.5-.3.8-.3H13c.6 0 1 .4 1 1Z"/>
+              </svg>
             )}
           </button>
         </span>
@@ -34,9 +38,13 @@ export default function GameStatsTab({ gameCode, gamePassword }) {
                 {gamePassword}
               </span>
               {copiedField === 1 ? (
-                <Image src="/checkmark.svg" alt="Copied" width={15} height={15} className="ml-2" />
+                <svg className="w-6 h-6 text-gray-800 dark:text-white ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m5 12 4.7 4.5 9.3-9"/>
+                </svg>
               ) : (
-                <Image src="/copy.svg" alt="Copy game code" width={15} height={15} className="ml-2" />
+                <svg className="w-6 h-6 text-gray-800 dark:text-white ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" strokeLinejoin="round" strokeWidth="2" d="M9 8v3c0 .6-.4 1-1 1H5m11 4h2c.6 0 1-.4 1-1V5c0-.6-.4-1-1-1h-7a1 1 0 0 0-1 1v1m4 3v10c0 .6-.4 1-1 1H6a1 1 0 0 1-1-1v-7.1c0-.3 0-.5.2-.7l2.5-2.9c.2-.2.5-.3.8-.3H13c.6 0 1 .4 1 1Z"/>
+                </svg>
               )}
             </button>
           </span>
