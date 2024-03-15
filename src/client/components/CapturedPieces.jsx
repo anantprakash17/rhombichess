289def76fd2c1f8e8fc5e26f7e0e39125d13c52a
Farzan Yazdanjou <farzan.yazdanjou@gmail.com>
2024-03-15 00:16:44 -0400

    Minor touch ups :)

--- src/client/components/CapturedPieces.jsx
+++ src/client/components/CapturedPieces.jsx
@@ -3,17 +3,21 @@ import Image from 'next/image';
 
 export default function CapturedPieces({ capturedPieces }) {
   return (
-    <div className="w-full bg-gray-600 rounded-lg m-2 p-1 scrollbar-custom overflow-y-auto">
-      {capturedPieces ? capturedPieces.map((piece, index) => (
-        <div key={index} className="inline-flex">
-          <Image 
-            src={`/pieces/${piece}.png`} 
-            alt={piece} 
-            width={38} 
+    <div className="w-full bg-gray-600 rounded-lg m-2 mr-0 p-1 scrollbar-custom overflow-y-auto">
+      {capturedPieces?.length ? capturedPieces.map((piece, index) => (
+        <div key={`${index}-${piece}`} className="inline-flex">
+          <Image
+            src={`/pieces/${piece}.png`}
+            alt={piece}
+            width={38}
             height={18}
           />
         </div>
-      )) : null}
+      )) : (
+        <p className="w-full flex text-xl h-full items-center justify-center text-gray-800">
+          No Captured Pieces
+        </p>
+      )}
     </div>
   );
 }
