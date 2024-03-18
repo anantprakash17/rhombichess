21901481624cf0c6b025010aa04decd04c368f52
bazinagm <bazinagm@mcmaster.ca>
2024-03-17 21:31:14 -0400

    Update Board.jsx

--- src/client/components/Board.jsx
+++ src/client/components/Board.jsx
@@ -7,6 +7,7 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
 import Tile from './Tile';
 import Piece from './Piece';
@@ -229,8 +230,8 @@ function Board({
       )}
       {gameData.promotion && (
         <PiecePromotionModal
+          color={"white"}
           open={piecePromotionModalOpen}
-          onClose={() => { setPiecePromotionModalOpen(false); }}
           gameCode={gameCode}
         />
       )}
@@ -295,11 +296,18 @@ export function CheckModal({ open, onClose }) {
   );
 }
 
-export function PiecePromotionModal({ open, onClose, gameCode }) {
-  const [selectedPiece, setSelectedPiece] = useState('Queen'); // Default to Queen
+export function PiecePromotionModal({ color, open, gameCode }) {
+  const [selectedPiece, setSelectedPiece] = useState('Queen');
+
+  const pieceNames = [
+    'queen', 'jester', 'rook', 'elephant',
+    'bishop', 'machine', 'dog', 'mammoth',
+    'cat', 'hawk', 'shield', 'knight', 'prince',
+  ];
+
 
   const handlePromotion = () => {
-    fetch(`http://${window.location.hostname}:8080/api/game/promotion/${gameCode}`, { // Replace with your API endpoint
+    fetch(`http://${window.location.hostname}:8080/api/game/promotion/${gameCode}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -310,7 +318,6 @@ export function PiecePromotionModal({ open, onClose, gameCode }) {
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
-        onClose();
       })
       .catch(error => {
         console.error('Failed to promote piece:', error);
@@ -318,36 +325,38 @@ export function PiecePromotionModal({ open, onClose, gameCode }) {
   };
 
   return (
-    <div className={`fixed inset-0 z-50 flex items-center justify-center ${open ? 'visible' : 'invisible'}`}>
-      <div className="relative bg-slate-600 rounded-lg shadow-xl p-6 m-4 max-w-sm max-h-full text-center z-50">
-        <button onClick={onClose} className="absolute top-0 right-0 p-2 mr-2 text-white text-2xl hover:text-gray-300" type="button">
-          &times;
-        </button>
-        <Logo />
-        <h2 className="text-center mx-2 m-1 mb-0 text-3xl font-bold text-white">
-          Promotion!
-        </h2>
-        <p className="mt-1 text-xl text-gray-300">
-          You can promote your pawn!
-        </p>
-        <select value={selectedPiece} onChange={e => setSelectedPiece(e.target.value)} className="mt-4 text-xl text-gray-300">
-          <option value="Queen">Queen</option>
-          <option value="Rook">Rook</option>
-          <option value="Bishop">Bishop</option>
-          <option value="Machine">Machine</option>
-          <option value="Knight">Knight</option>
-          <option value="Shield">Shield</option>
-          <option value="Jester">Jester</option>
-          <option value="Cat">Cat</option>
-          <option value="Elephant">Elephant</option>
-          <option value="Prince">Prince</option>
-          <option value="Dog">Dog</option>
-          <option value="Mammoth">Mammoth</option>
-          <option value="Hawk">Hawk</option>
-        </select>
-        <button onClick={handlePromotion} className="mt-4 p-2 text-white text-xl hover:text-gray-300" type="button">
-          Promote
-        </button>
+    <div className={`fixed inset-0 z-50 ${open ? 'visible' : 'invisible'}`}>
+      <div className={`fixed bg-slate-600 rounded-md shadow-lg p-2 border-2 border-gray-700 flex items-center space-x-2 transition-transform duration-300 ease-in-out ${open ? 'translate-x-0 opacity-100' : '-translate-x-full opacity-0'}`}>
+        <form onSubmit={handlePromotion}>
+          <div className="flex-col items-center justify-center w-full bg-slate-600">
+            <div className="flex-col items-center justify-center">
+              <ul>
+                {pieceNames.map((pieceName) => (
+                  <li key={pieceName}>
+                    <input
+                      type="radio"
+                      id={pieceName}
+                      name="piecePromotion"
+                      value={pieceName}
+                      className="hidden peer"
+                      required
+                      checked={selectedPiece === pieceName}
+                      onChange={(event) => setSelectedPiece(event.target.value)}
+                    />
+                    <label htmlFor={pieceName} className="block relative rounded-lg p-1 m-1 bg-gray-500 hover:bg-gray-100 focus:bg-gray-300 cursor-pointer peer-checked:bg-gray-100">
+                      <Image src={`/pieces/${pieceName}-${color}.png`} alt={`${pieceName}-${color}`} width={40} height={30} className="mx-auto" />
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <button className="relative text-xl p-2 m-1 rounded-lg font-semibold bg-green-500 text-white hover:bg-green-600 focus:bg-green-700" type="submit">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 507.68" width="40" height="30" >
+              <path fill="#f7fafc" d="M442.17 335.59c5.89-13.07 21.27-18.89 34.34-13 13.07 5.89 18.89 21.26 13 34.33-20.4 45.13-53.52 83.3-94.8 109.95-39.99 25.82-87.59 40.8-138.63 40.8-70.71 0-134.73-28.66-181.07-75S0 322.3 0 251.59C0 193.6 19.32 140.08 51.88 97.12c33.29-43.93 80.51-76.81 135.14-92.1 13.8-3.81 28.08 4.29 31.89 18.09 3.81 13.8-4.28 28.08-18.08 31.89-43.37 12.14-80.94 38.34-107.51 73.4-25.93 34.21-41.31 76.89-41.31 123.19 0 56.36 22.84 107.38 59.77 144.31 36.92 36.92 87.94 59.77 144.3 59.77 40.8 0 78.77-11.93 110.6-32.48 32.85-21.21 59.24-51.63 75.49-87.6zm-215.02 22.59h57.86c11.44 0 20.82-9.38 20.82-20.82v-73.87h40.7c6.93-.3 11.86-2.6 14.7-6.92 7.72-11.57-2.81-23-10.12-31.05-20.74-22.76-71.56-77.45-81.79-89.49-7.76-8.58-18.8-8.58-26.56 0-10.57 12.34-63.94 69.53-83.66 91.67-6.84 7.7-15.29 18.21-8.17 28.87 2.91 4.32 7.78 6.62 14.72 6.92h40.68v73.87c0 11.44 9.37 20.82 20.82 20.82zm69.3-306.68c-14.08-2.8-23.22-16.49-20.41-30.57 2.8-14.08 16.49-23.22 30.57-20.42C364.7 12.15 415.7 43.46 452.37 87.2c36.04 42.99 58.22 98.1 59.62 158.42.28 14.3-11.09 26.13-25.39 26.41-14.3.28-26.13-11.09-26.41-25.39-1.11-47.89-18.83-91.77-47.63-126.12-29.19-34.81-69.82-59.74-116.11-69.02z"/>
+            </svg>
+          </button>
+        </form>
       </div>
     </div>
   );
