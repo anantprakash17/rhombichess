04785fe9aa6052840d5f1b30574382ad9a00dbf4
Anant Prakash <anantprakash92@gmail.com>
2024-03-24 23:06:36 -0400

    feat: switch to prod backend server

--- src/server/server.py
+++ src/server/server.py
@@ -1,7 +1,20 @@
+import logging
+from gevent.pywsgi import WSGIServer
+from geventwebsocket.handler import WebSocketHandler
 from flask_cors import CORS
 from app import app, socketio
 
 CORS(app)
 
+# log errors to stderr with gevent
+logging.basicConfig(level=logging.ERROR, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+log = logging.getLogger(__name__)
+
 if __name__ == "__main__":
-    socketio.run(app, port=8080, host="0.0.0.0", debug=True)
+    http_server = WSGIServer(("0.0.0.0", 8080), app, handler_class=WebSocketHandler, log=log)
+    try:
+        http_server.serve_forever()
+    except KeyboardInterrupt:
+        log.info("Shutting down server")
+        http_server.stop()
+        log.info("Server stopped")
